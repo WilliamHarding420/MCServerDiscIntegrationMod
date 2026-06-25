fbb5242a673458574348020fa2dcbd00027bcca9
Will <pcgamigwill@gmail.com>
2026-06-25 02:00:24 +0100

    Webhook events for death and advancement methods

--- src/main/java/dev/will/twg/events/PlayerEvents.java
+++ src/main/java/dev/will/twg/events/PlayerEvents.java
@@ -4,10 +4,13 @@ import dev.will.twg.Config;
 import dev.will.twg.DiscordWebHook;
 import dev.will.twg.WebHookHelper;
 import dev.will.twg.discord.DiscordBot;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.ServerChatEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.player.AdvancementEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 public class PlayerEvents {
@@ -55,6 +58,37 @@ public class PlayerEvents {
 
         if(Config.SEND_MINECRAFT_MESSAGES.get())
             WebHookHelper.SendWebHook(WebHookHelper.DiscordWebHookURL, webHookMessage);
+
+    }
+
+    @SubscribeEvent
+    public void onPlayerAdvancement(AdvancementEvent.AdvancementEarnEvent event) {
+
+        DiscordWebHook.LOGGER.debug("Executing player advancement event.");
+
+        Player player = event.getEntity();
+
+        String webHookMessage = String.format("%s completed advancement %s.",
+                player.getDisplayName().getString(),
+                event.getAdvancement().value().name().get().getString());
+
+        if (Config.SEND_ADVANCEMENT_MESSAGES.get())
+            WebHookHelper.SendWebHook(WebHookHelper.DiscordWebHookURL, webHookMessage);
+
+    }
+
+    @SubscribeEvent
+    public void onPlayerDeath(LivingDeathEvent event) {
+
+        if (!(event.getEntity() instanceof Player))
+            return;
+
+        DiscordWebHook.LOGGER.debug("Executing living death event.");
+
+        Component deathMessage = event.getSource().getLocalizedDeathMessage(event.getEntity());
+
+        if (Config.SEND_DEATH_MESSAGES.get())
+            WebHookHelper.SendWebHook(WebHookHelper.DiscordWebHookURL, String.format("%s.", deathMessage.getString()));
 
     }
 
